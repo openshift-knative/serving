309099dab5b8de3af4ee56699a958a5ebf11998e
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-12-15 16:40:22 +0900

    Drop duplicate manifests

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -4,14 +4,15 @@ root="$(dirname "${BASH_SOURCE[0]}")"
 
 source $(dirname $0)/resolve.sh
 
-release=$1
-output_file="openshift/release/knative-serving-${release}.yaml"
+readonly YAML_OUTPUT_DIR="openshift/release/artifacts/"
 
-resolve_resources "config/core/ config/hpa-autoscaling/" "$output_file"
+readonly SERVING_CRD_YAML=${YAML_OUTPUT_DIR}/1-serving-crds.yaml
+readonly SERVING_CORE_YAML=${YAML_OUTPUT_DIR}/2-serving-core.yaml
+readonly SERVING_HPA_YAML=${YAML_OUTPUT_DIR}/3-serving-hpa.yaml
+readonly SERVING_POST_INSTALL_JOBS_YAML=${YAML_OUTPUT_DIR}/4-serving-post-install-jobs.yaml
 
-if [[ "$release" != "ci" ]]; then
-  # Drop the "knative-" suffix, which is added in upstream branch.
-  # e.g. knative-v1.7.0 => v1.7.0
-  release=${release#"knative-"}
-  ${root}/download_release_artifacts.sh $release
-fi
+# Generate Knative component YAML files
+resolve_resources "config/core/300-resources/ config/core/300-imagecache.yaml" "$SERVING_CRD_YAML"
+resolve_resources "config/core/"                                               "$SERVING_CORE_YAML"
+resolve_resources "config/hpa-autoscaling/"                                    "$SERVING_HPA_YAML"
+resolve_resources "config/post-install/storage-version-migration.yaml"         "$SERVING_POST_INSTALL_JOBS_YAML"
