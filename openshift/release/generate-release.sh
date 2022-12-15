c9207d3fcf2ede74a294ca3487e8f1fee4fa0784
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-12-15 13:54:55 +0900

    Fix comment

--- openshift/release/generate-release.sh
+++ openshift/release/generate-release.sh
@@ -6,18 +6,18 @@ source $(dirname $0)/resolve.sh
 
 readonly YAML_OUTPUT_DIR="openshift/release/artifacts/"
 
-# Generated Knative component YAML files
 readonly SERVING_CRD_YAML=${YAML_OUTPUT_DIR}/1-serving-crds.yaml
 readonly SERVING_CORE_YAML=${YAML_OUTPUT_DIR}/2-serving-core.yaml
 readonly SERVING_HPA_YAML=${YAML_OUTPUT_DIR}/3-serving-hpa.yaml
 readonly SERVING_POST_INSTALL_JOBS_YAML=${YAML_OUTPUT_DIR}/4-serving-post-install-jobs.yaml
 
+# Generate Knative component YAML files
 resolve_resources "config/core/300-resources/ config/core/300-imagecache.yaml" "$SERVING_CRD_YAML"
 resolve_resources "config/core/"                                               "$SERVING_CORE_YAML"
 resolve_resources "config/hpa-autoscaling/"                                    "$SERVING_HPA_YAML"
 resolve_resources "config/post-install/storage-version-migration.yaml"         "$SERVING_POST_INSTALL_JOBS_YAML"
 
-# generates a single yaml for the CI.
+# Generate a single yaml for the CI.
 release=$1
 output_file="openshift/release/knative-serving-${release}.yaml"
 resolve_resources "$YAML_OUTPUT_DIR" "$output_file"
